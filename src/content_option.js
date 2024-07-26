fcd139bd02977b4c9f7879d9b98715c9c7fcb761
Vivi0603 <101891372+Vivi0603@users.noreply.github.com>
2024-07-26 12:24:45 -0400

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,162 @@
+const logotext = "VIVIAN";
+const meta = {
+    title: "Vivian Dmello",
+    description: "I’m Vivian Dmello, a data analyst specializing in business intelligence, data visualization, and predictive modeling.",
+};
+
+const introdata = {
+    title: "I’m Vivian Dmello",
+    animated: {
+        first: "I love data analysis",
+        second: "I create insightful dashboards",
+        third: "I develop predictive models",
+    },
+    description: "Data Analyst with 3+ years of experience in business intelligence, data visualization, and predictive modeling. Proficient in SQL, Python, and BI tools.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A bit about myself",
+    aboutme: "Data Analyst with a track record of delivering data-driven solutions that enhance decision-making processes. Skilled in leveraging analytics tools to drive actionable insights and improve business efficiency.",
+};
+const worktimeline = [{
+        jobtitle: "Data Analyst Intern",
+        where: "DataDated.com",
+        date: "January 2024 - June 2024",
+    },
+    {
+        jobtitle: "Data Analyst Intern",
+        where: "Datacrowd LLC",
+        date: "September 2023 - December 2023",
+    },
+    {
+        jobtitle: "Data Analyst",
+        where: "Active Infotech",
+        date: "August 2021 - July 2023",
+    },
+    {
+        jobtitle: "Data Analyst",
+        where: "Supreme Web Tech",
+        date: "December 2020 - July 2021",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "SQL",
+        value: 85,
+    },
+    {
+        name: "JavaScript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Tableau",
+        value: 85,
+    },
+    {
+        name: "Power BI",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Data Analysis",
+        description: "Proficient in leveraging data analytics tools to drive actionable insights and improve business efficiency.",
+    },
+    {
+        title: "Data Visualization",
+        description: "Skilled in developing interactive dashboards to enhance data-driven decision-making.",
+    },
+    {
+        title: "Predictive Modeling",
+        description: "Experienced in building predictive models to forecast trends and inform strategic decisions.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Developed interactive Tableau dashboards for a sales team, reducing report generation time by 40%.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "Implemented demand forecasting models with 85% accuracy, reducing holding costs by 10%.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Engineered a real-time anomaly detection system, reducing fraudulent transactions by 30%.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Led migration of on-premises data warehouse to Amazon Redshift, decreasing data access latency by 40%.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "Enhanced web analytics accuracy by 25% for a SaaS platform.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "Spearheaded development of Power BI dashboards for marketing performance.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Implemented data quality control measures, decreasing errors in marketing datasets by 35%.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "Developed a recommendation engine, increasing cross-sell revenue by 20%.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Designed and deployed an automated ETL pipeline, increasing data refresh frequency by 200%.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "Conducted SQL-based cohort analysis, boosting customer retention by 12%.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "viviandmello63@gmail.com",
+    YOUR_FONE: "(929) 687-6918",
+    description: "Feel free to reach out to discuss data analytics, predictive modeling, or any exciting opportunities. I'm always open to connecting with like-minded professionals.",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com/in/viviandmello0603",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
